72ce7a145258952a18e7d2bfbba41908d83021f6
lonejavid <lonejavid0739@gmail.com>
2020-03-10 17:16:08 +0530

    [javid]added.program based on regular expression added

--- MainPrograms/regularExpression.php
+++ MainPrograms/regularExpression.php
@@ -12,10 +12,20 @@
 * @since : 06-03-2020
 */
 include 'C:\Users\sneha\Desktop\ObjectOriented Programming\Utilty\utility.php';
-$str="Hello <<name>>, We have your full
-name as <<full name>> in our system. your contact number is 91­xxxxxxxxxx.
-Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.";
-echo "Enter the valid name \n";
-$name=Utility::validateName();
-echo "Enter the valid FullName \n";
-echo "Enter the valid 10 digit ";
+$str="Hello <<name>>, We have your full 
+name as <<full name>> in our system. your contact number is 91­-xxxxxxxxxx.
+Please,let us know in case of any clarification Thank you BridgeLabz xx/xx/xxxx.";
+echo "enter valid name: ";
+$name=Utility::nameValidation();
+echo "\nenter valid full name: ";
+$fName=Utility::fullNameValidation();
+echo $fName,"\n";
+echo "\nenter valid mobile Number: ";
+$number=Utility::numberValidation();
+$date=date("d/m/Y");
+$str=str_replace("<<name>>",$name,$str);
+$str=str_replace("<<full name>>",$fName,$str);
+$str=str_replace("xxxxxxxxxx",$number,$str);
+$str=str_replace("xx/xx/xxxx",$date,$str);
+echo $str,"\n\n";
+?>
